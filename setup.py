8dc7242d9d8273c899711ffabfab9ba615aa271b
Eric Julià <59866781+erju-dev@users.noreply.github.com>
2024-04-05 11:59:19 +0200

    Creamos fichero con errores: setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#-- Imports --#
+import sys
+
+
+#-- Functions
+def calculadora_suma(num1):
+    return num1 + num2
+
+def main():
+    print("Bienvenidos a la calculadora de sumas")
+    num1 = int(input("Introduce numero 1: ")
+    num2 = int(input("Introduce numero 2: "))
+    print()
+    print(f"El resultado es: {calculadora_suma(num1, num2)}")
+
+#-- Main --#
+if __name__ == "__main__":
+    os.system("cls")
+    main()
