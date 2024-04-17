f19c96c922081fc87127507f92783cd59fea9fb9
Eric Julià <59866781+erju-dev@users.noreply.github.com>
2024-04-17 12:08:05 +0200

    añadir errores

--- eric_error.py
+++ eric_error.py
@@ -1,7 +1,5 @@
 #-- Imports --#
 import sys
-import os
-
 
 #-- Functions
 def calculadora_suma(num1):
@@ -9,10 +7,10 @@ def calculadora_suma(num1):
 
 def main():
     print("Bienvenidos a la calculadora de sumas")
-    #num1 = int(input("Introduce numero 1: "))
-    #num2 = int(input("Introduce numero 2: "))
-    num1 = 3
-    num2 = 3
+    num1 = int(input("Introduce numero 1: ")
+    num2 = int(input("Introduce numero 2: "))
+    #num1 = 3
+    #num2 = 3
     print()
     print(f"El resultado es: {calculadora_suma(num1, num2)}")
 
