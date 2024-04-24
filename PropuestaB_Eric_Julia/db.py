424df7f896b2170b793581cec89b4b661bc69889
Eric Julià <59866781+erju-dev@users.noreply.github.com>
2024-04-24 16:01:09 +0200

    udpate2

--- PropuestaB_Eric_Julia/db.py
+++ PropuestaB_Eric_Julia/db.py
@@ -2,8 +2,7 @@
 # (Seguridad, funcionalidades...)
 
 # Importamos modulos
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import declarative_base
 
 # (1) Creamos el engine, el cual permite a SQLAlchemy
 #     comunicarse con la DB en un dialecto concreto
