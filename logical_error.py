eaa2ea32ed4bb7b58f8e8ddb26403909a8f89e55
Eric Julià <59866781+erju-dev@users.noreply.github.com>
2024-05-08 11:20:17 +0200

    Update logical_error.py

--- logical_error.py
+++ logical_error.py
@@ -1,22 +1,21 @@
 #-- Imports --#
 import sys
-import os
 
 #-- Functions
 def main2():
     cara = int(input("Introduce cara para calcular area: "))
-    area = cara * cara  # Corrected area calculation for a square
+    area = cara * cara  * cara
     print("El area es:", area)
 
 def main3():
     tiempo = 20
     if tiempo > 30:
-        print("Estamos por encima de 30 grados")
+        print("Estamos a 30 grados")
     else:
         print("No estamos por encima de 30 grados")
 
 #-- Main --#
 if __name__ == "__main__":
-    os.system("cls" if os.name == "nt" else "clear")
+    os.system("cls")
     main2()
     main3()
